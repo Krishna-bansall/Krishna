8808bfee56250e85e0fe86087d546f4fafe68d6f
krishna-bansall <bansalkrishna321go@gmail.com>
2022-12-02 00:45:19 +0530

    Basic Structure Added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				main: ['Poppins', 'sans-serif'],
+				mono: ['Ubuntu Mono', 'monospace'],
+				display: ['Clash Display', 'sans-serif'],
+			  },
+		},
 	},
 	plugins: [],
 }
